c00d1977e58030ddd59983e99dc98b52f477f0d5
sho <blaziyt@gmail.com>
2020-05-27 16:53:36 -0500

    Add home page & react router

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './home/Home.js';
+
+const Main = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Home}/>
+        </Switch>
+    )
+};
+
+export default Main;
