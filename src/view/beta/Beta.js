9f6667ab6c99be52ef35507418123c32e72e7304
sho <blaziyt@gmail.com>
2020-07-29 21:20:44 -0500

    Change things on the Beta page.

--- src/view/beta/Beta.js
+++ src/view/beta/Beta.js
@@ -31,14 +31,15 @@ export default function Beta() {
             <p className="max-w-lg">
                 Unifey is currently in it's alpha stages. This means features
                 are definitely not in working order, and there are too many bugs
-                to count. However, in the near future we will have a limited
-                beta run. What this means is that we'll allow specific users to
+                to count. However, in the near future we will have a 
+                beta run. What this means is that we'll allow users who have signed up to
                 use Unifey to it's extent to attempt to find bugs.
                 <br />
                 <br />
                 If you'd like to join and help us in beta, please sign up below.
                 When you do sign up, you will be notified through email when
-                beta begins. This email will include the account created for you.
+                beta begins. This email will include the account created for
+                you.
             </p>
 
             <p className="text-2xl">Beta Signup</p>
@@ -46,7 +47,8 @@ export default function Beta() {
                 This is the area to sign-up for beta access. Make sure both your
                 email and username have not been used in a previous beta signup.
                 Remember: Unless you verify your email, the signup will not be
-                available on beta and other users may potentially be able to take your name.
+                available on beta and other users may potentially be able to
+                take your name.
             </p>
 
             <div className="mt-8">
@@ -105,6 +107,23 @@ export default function Beta() {
                             <Checkbox>
                                 I accept the <a href="/tos">Privacy Policy</a>.
                             </Checkbox>
+                        </Form.Item>
+
+                        <Form.Item
+                            name="email-policy"
+                            valuePropName="checked"
+                            rules={[
+                                {
+                                    validator: (_, value) =>
+                                        value
+                                            ? Promise.resolve()
+                                            : Promise.reject(
+                                                  "You must accept the email policy."
+                                              ),
+                                },
+                            ]}
+                        >
+                            <Checkbox>I understand I will be emailed to verify my beta account.</Checkbox>
                         </Form.Item>
 
                         <Form.Item
