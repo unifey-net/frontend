88c42e110a02686eaab9956b6d8e6102baf4b903
sho <blaziyt@gmail.com>
2021-07-02 23:17:55 -0500

    Join community button fits proportionally

--- src/components/community/profile/JoinCommunity.tsx
+++ src/components/community/profile/JoinCommunity.tsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import { media } from "../../../api/util/Media";
 
 const JoinCommunity = styled.button`
     background-color: #bf1d48;
     border: none;
     border-radius: 4px;
+
+    ${media(`font-size: 12px;`, `font-size: 16px;`, `font-size: 16px;`)}
 `
 
 export default JoinCommunity
