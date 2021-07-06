465ec1ac641b71d72f9a5a4d9887ab8ec337eb0a
sho <blaziyt@gmail.com>
2021-07-06 00:56:06 -0500

    Change post date depending on time posted

--- src/components/feed/post/PostAbout.tsx
+++ src/components/feed/post/PostAbout.tsx
@@ -1,12 +1,24 @@
 import React from "react"
 
+const DAY = 1000 * 60 * 60 * 24
+
+const properDate = (time: number): string => {
+    const current = Date.now()
+    const timeDate = new Date(time)
+
+    if (current - time >= DAY)
+        return timeDate.toLocaleDateString()
+    else
+        return timeDate.toLocaleTimeString()
+}
+
 /**
  * About a post.
  */
 const PostAbout: React.FC<{ date: number }> = ({ date }) => {
     return (
         <span className="post-creation-date">
-            {new Date(date).toLocaleString()}
+            {properDate(date)}
         </span>
     )
 }
