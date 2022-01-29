25a05eb00b5a078a91399d3201805e711d59ba62
Shoganeko <blaziyt@gmail.com>
2022-01-29 02:03:09 -0600

    Update mobile appearance for default homepage

--- src/components/home/DefaultHome.tsx
+++ src/components/home/DefaultHome.tsx
@@ -1,56 +1,69 @@
 import React from "react"
 import styled from "styled-components"
+import { desktopMedia, media } from "../../api/util/Media"
 
 const Home = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  img {
+    ${media("transform: scale(0.5);", "transform: scale(0.75);", "transform: scale(1.0);")}
+  }
+
+  .questions {
     display: flex;
-    align-items: center;
-    justify-content: center;
     flex-direction: column;
-
-    .questions {
-        display: flex;
-        flex-direction: row;
-        gap: 2rem;
-
-        p {
-            overflow-wrap: break-word;
-            max-width: 20rem;
-        }
+    gap: 2rem;
+    
+    ${media("", "flex-direction: row;", "flex-direction: row;")}
+    
+    div {
+      max-width: 150px;
+      padding-left: 16px;
+      padding-right: 16px;
     }
+
+    p {
+      overflow-wrap: break-word;
+      max-width: 20rem;
+    }
+  }
 `
 
 const DefaultHome: React.FC = () => {
     return (
         <Home>
             <div>
-                <img src="/homepage.png" alt="Large Unifey Logo"></img>
+                <img src="/homepage.png" alt="Large Unifey Logo" />
             </div>
 
-                <div className="questions">
-                    <div>
-                        <h2>What is Unifey?</h2>
-                        <p>
-                            Unifey is an open source social-media platform,
-                            dedicated to freely exchange ideas.
-                        </p>
-                    </div>
-
-                    <div className="learn-more">
-                        <h2>How can I learn more?</h2>
-                        <p>
-                            You can join our
-                            <a
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://discord.gg/e9wKgAt"
-                            >
-                                {" "}
-                                Discord
-                            </a>
-                            .
-                        </p>
-                    </div>
+            <div className="questions">
+                <div>
+                    <h2>What is Unifey?</h2>
+                    <p>
+                        Unifey is an open source social-media platform,
+                        dedicated to freely exchange ideas.
+                    </p>
                 </div>
+
+                <div className="learn-more">
+                    <h2>How can I learn more?</h2>
+                    <p>
+                        You can join our
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://discord.gg/e9wKgAt"
+                        >
+                            {" "}
+                            Discord
+                        </a>
+                        .
+                    </p>
+                </div>
+            </div>
         </Home>
     )
 }
